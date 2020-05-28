6070792346923f5f0eb809cabcebafc8265629af
Konrad Mrożek <mrozek.konrad@gmail.com>
2020-05-28 22:20:58 +0200

    Use new value objects in new runner implementation

--- application/theatre/runner.php
+++ application/theatre/runner.php
@@ -1,43 +1,50 @@
 <?php
 
-function statement(array $invoice, array $plays)
+require_once 'vendor/autoload.php';
+
+use Theatre\Customer;
+use Theatre\Invoice;
+use Theatre\Performance;
+use Theatre\Performances;
+
+function statement(Invoice $invoice, array $plays)
 {
     $totalAmount   = 0;
     $volumeCredits = 0;
 
-    $invoiceCustomer = $invoice['customer'];
+    $invoiceCustomer = $invoice->customer()->name();
 
     $result = "Rachunek dla $invoiceCustomer" . PHP_EOL;
 
-    foreach ($invoice['performances'] as $performance) {
-        $play   = $plays[$performance['playId']];
+    foreach ($invoice->performances() as $performance) {
+        $play   = $plays[$performance->playId()];
         $amount = 0;
 
         switch ($play['type']) {
             case "tragedy":
                 $amount = 40000;
-                if ($performance['audience'] > 30) {
-                    $amount += 1000 * ($performance['audience'] - 30);
+                if ($performance->audience() > 30) {
+                    $amount += 1000 * ($performance->audience() - 30);
                 }
                 break;
             case "comedy":
                 $amount = 30000;
-                if ($performance['audience'] > 20) {
-                    $amount += 10000 + 500 * ($performance['audience'] - 20);
+                if ($performance->audience() > 20) {
+                    $amount += 10000 + 500 * ($performance->audience() - 20);
                 }
-                $amount += 300 * $performance['audience'];
+                $amount += 300 * $performance->audience();
                 break;
             default:
                 throw new Exception('Unknown audience type ' . $play['type']);
         }
 
-        $volumeCredits += max($performance['audience'] - 30, 0);
+        $volumeCredits += max($performance->audience() - 30, 0);
 
         if ("comedy" === $play['type']) {
-            $volumeCredits += floor($performance['audience'] / 5);
+            $volumeCredits += floor($performance->audience() / 5);
         }
 
-        $result      .= ' ' . $play['name'] . ': ' . number_format($amount / 100) . ' (liczba miejsc:' . $performance['audience'] . ')' . PHP_EOL;
+        $result      .= ' ' . $play['name'] . ': ' . number_format($amount / 100) . ' (liczba miejsc:' . $performance->audience() . ')' . PHP_EOL;
         $totalAmount += $amount;
     }
 
@@ -51,5 +58,13 @@ $invoices = json_decode(file_get_contents(__DIR__ . '/json/invoices.json'), true
 $plays    = json_decode(file_get_contents(__DIR__ . '/json/plays.json'), true);
 
 foreach ($invoices as $invoice) {
+    $performances = [];
+
+    foreach ($invoice['performances'] as $performance) {
+        $performances[] = new Performance($performance['playId'], $performance['audience']);
+    }
+
+    $invoice = new Invoice(new Customer($invoice['customer']), new Performances(...$performances));
+
     echo statement($invoice, $plays) . PHP_EOL;
 }
